b5f94809e18627c5abad3279270ad3e044a8fc89
Samuel Wu <twopizza9621536@gmail.com>
2023-08-31 12:24:52 -0400

    feat(lualine): finish the light theme
    Signed-off-by: Samuel Wu <twopizza9621536@gmail.com>

--- lua/lualine/themes/bubblegum-256-light.lua
+++ lua/lualine/themes/bubblegum-256-light.lua
@@ -1,8 +1,8 @@
 local colors = {
 	white = "#FFFFFF",
-	med_gray_hi = "#E4E4E4",
-	med_gray_lo = "#3A3A3A",
 	gray1 = "#626262",
+	gray2 = "#E4E4E4",
+	gray3 = "#D7D7D7",
 	green = "#AFD787",
 	blue = "#87AFB7",
 	purple = "#AFAFD7",
@@ -15,29 +15,29 @@ local bubblegum = {}
 
 bubblegum.normal = {
 	a = { bg = colors.green, fg = colors.white },
-	b = { bg = colors.med_gray_lo, fg = colors.gray1 },
-	c = { bg = colors.med_gray_hi, fg = colors.green },
+	b = { bg = colors.gray3, fg = colors.gray1 },
+	c = { bg = colors.gray2, fg = colors.green },
 }
 
 bubblegum.insert = {
-	a = { bg = colors.blue, fg = colors.med_gray_hi },
-	c = { bg = colors.med_gray_hi, fg = colors.blue },
+	a = { bg = colors.blue, fg = colors.gray2 },
+	c = { bg = colors.gray2, fg = colors.blue },
 }
 
 bubblegum.visual = {
 	a = { bg = colors.pink, fg = colors.white },
-	c = { bg = colors.med_gray_hi, fg = colors.pink },
+	c = { bg = colors.gray2, fg = colors.pink },
 }
 
 bubblegum.replace = {
 	a = { bg = colors.red, fg = colors.white },
-	c = { bg = colors.med_gray_hi, fg = colors.red },
+	c = { bg = colors.gray2, fg = colors.red },
 }
 
 bubblegum.inactive = {
-	a = { bg = colors.med_gray_hi, fg = colors.gray1 },
-	b = { bg = colors.med_gray_hi, fg = colors.gray1, gui = "bold" },
-	c = { bg = colors.med_gray_hi, fg = colors.gray1 },
+	a = { bg = colors.med_gray2, fg = colors.gray1 },
+	b = { bg = colors.med_gray2, fg = colors.gray1, gui = "bold" },
+	c = { bg = colors.med_gray2, fg = colors.gray1 },
 }
 
 return bubblegum

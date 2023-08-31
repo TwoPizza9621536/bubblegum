13dd25a7acf78a106f4906ac0167353c6e9f2f10
Samuel Wu <twopizza9621536@gmail.com>
2023-08-31 11:53:21 -0400

    feat: Add lualine themes
    Signed-off-by: Samuel Wu <twopizza9621536@gmail.com>

---
+++ lua/lualine/themes/bubblegum-256-dark.lua
@@ -0,0 +1,43 @@
+local colors = {
+	dark_gray = "#303030",
+	med_gray_hi = "#444444",
+	med_gray_lo = "#3A3A3A",
+	light_gray = "#B2B2B2",
+	green = "#AFD787",
+	blue = "#87AFD7",
+	purple = "#AFAFD7",
+	orange = "#D7AF5F",
+	red = "#D78787",
+	pink = "#D7AFD7",
+}
+
+local bubblegum = {}
+
+bubblegum.normal = {
+	a = { bg = colors.green, fg = colors.dark_gray },
+	b = { bg = colors.med_gray_lo, fg = colors.light_gray },
+	c = { bg = colors.med_gray_hi, fg = colors.green },
+}
+
+bubblegum.insert = {
+	a = { bg = colors.blue, fg = colors.med_gray_hi },
+	c = { bg = colors.med_gray_hi, fg = colors.blue },
+}
+
+bubblegum.visual = {
+	a = { bg = colors.pink, fg = colors.dark_gray },
+	c = { bg = colors.med_gray_hi, fg = colors.pink },
+}
+
+bubblegum.replace = {
+	a = { bg = colors.red, fg = colors.dark_gray },
+	c = { bg = colors.med_gray_hi, fg = colors.red },
+}
+
+bubblegum.inactive = {
+	a = { bg = colors.med_gray_hi, fg = colors.light_gray },
+	b = { bg = colors.med_gray_hi, fg = colors.light_gray, gui = "bold" },
+	c = { bg = colors.med_gray_hi, fg = colors.light_gray },
+}
+
+return bubblegum
